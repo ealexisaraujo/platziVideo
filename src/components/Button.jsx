46a15c262c9c81bb62e6ce118eb29fe9c11db39a
Alexis Araujo <ala1289@gmail.com>
2019-09-24 07:51:30 -0500

    State - Events

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const Button = props => {
-  const { text } = props;
-  return (
-    <div>
-      <button type="button">{props.text}</button>
-      <button type="button">{text}</button>
-    </div>
-  );
-};
+class Button extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleClick = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Manzanas: {count}</h1>
+        <button type="button" onClick={this.handleClick}>
+          Agregar
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Button;
