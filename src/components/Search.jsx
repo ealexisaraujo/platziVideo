ae2c1bc41ec7c5dc4977e94891450c8c733eef0f
Alexis Araujo <ala1289@gmail.com>
2019-09-24 13:24:24 -0500

    Estructura y Estilos de Carousel y Carousel Item

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,7 +7,7 @@ const maininputplaceholder = 'Buscar...';
 const Search = () => (
   <section className='main'>
     <h2 className='main__title'>{maintitletext}</h2>
-    <input type='text' className='input' placeholder='{maininputplaceholder}' />
+    <input type='text' className='input' placeholder={maininputplaceholder} />
   </section>
 );
 
