0cbfeee6e4a294d4f4d10f3c2ec9cca49260f5dd
Alexis Araujo <ala1289@gmail.com>
2019-09-26 10:34:15 -0500

    Container: 404 Not Found

---
+++ src/containers/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/components/NotFound.scss';
+
+const NotFound = () => (
+  <section className='error'>
+    <h1>404</h1>
+    <p>Page not found</p>
+  </section>
+);
+
+export default NotFound;
