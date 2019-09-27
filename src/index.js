2783333315948310246907147bdbb6f48e32c34a
Alexis Araujo <ala1289@gmail.com>
2019-09-27 12:55:11 -0500

    Instalacion de Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactDom.render(<App />, document.getElementById('app'));
+ReactDom.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
