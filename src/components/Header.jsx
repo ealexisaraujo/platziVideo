6ad67daaa75b571e88fa89acb01377b5f2d72f26
Alexis Araujo <ala1289@gmail.com>
2019-09-30 10:48:37 -0500

    Creando y usando gravatar en nuestro proyecto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
 
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
-  <header className='header'>
-    <Link to='/'>
-      <img className='header__img' src={logo} alt='Platzi Video' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='' />
-        <p>Perfil</p>
+const Header = props => {
+  const { user } = props;
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          <img
+            src={Object.keys(user).length > 0 ? gravatar(user.email) : userIcon}
+            alt={user.email}
+          />
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <a href='/'>Cuenta</a>
+          </li>
+          <li>
+            <Link to='/login'>Iniciar Sesión</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <a href='/'>Cuenta</a>
-        </li>
-        <li>
-          <Link to='/login'>Iniciar Sesión</Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(Header);
--- src/index.js
+++ src/index.js
@@ -171,7 +171,11 @@ const initialState = {
   ]
 };
 
-const store = createStore(reducer, initialState);
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDom.render(
   <Provider store={store}>
