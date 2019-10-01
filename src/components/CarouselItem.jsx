47402f824580e5a2ac8b1080463f2606600475f3
Alexis Araujo <ala1289@gmail.com>
2019-10-01 17:46:48 -0500

    Vista general del player

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setFavorite, deleteFavorite } from '../actions';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
@@ -28,11 +29,13 @@ const CarouselItem = props => {
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
-          <img
-            className='carousel-item__details--img'
-            src={playIcon}
-            alt='Play Icon'
-          />
+          <Link to={`/player/${id}`}>
+            <img
+              className='carousel-item__details--img'
+              src={playIcon}
+              alt='Play Icon'
+            />
+          </Link>
           {isList ? (
             <img
               className='carousel-item__details--img'
