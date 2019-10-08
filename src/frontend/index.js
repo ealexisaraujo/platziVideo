b7b69db81f9d8a924cf7fd155e56958745d7f8d3
Alexis Araujo <ala1289@gmail.com>
2019-10-08 10:24:57 -0500

    Aplicando history y creando rutas para el servidor

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
 import { createStore, compose } from 'redux';
 import reducer from './reducers';
 import App from './routes/App';
@@ -174,10 +176,13 @@ const initialState = {
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducer, initialState, composeEnhancers());
+const history = createBrowserHistory();
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('app'),
 );
