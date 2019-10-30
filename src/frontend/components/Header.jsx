acf543265b00420fac5d3883a6e541b684c46acd
Alexis Araujo <ala1289@gmail.com>
2019-10-30 17:21:46 -0600

    Fetch de estado inicial

--- src/frontend/components/Header.jsx
+++ src/frontend/components/Header.jsx
@@ -22,6 +22,7 @@ const Header = props => {
     document.cookie = 'email=';
     document.cookie = 'name=';
     document.cookie = 'id=';
+    document.cookie = 'token=';
     props.logoutRequest({});
     window.location.href = '/login';
   };
